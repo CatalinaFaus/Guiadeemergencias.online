b2c132d5b77ed3b858ef1ba3ef93ab1e10e8ab4e
CatalinaFaus <catalinaafaus@gmail.com>
2024-11-11 12:45:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Validación simple de formulario
+// Desplegable para el menú
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+});
+
+// Validación del formulario
 document.getElementById('form-reportar').addEventListener('submit', function(e) {
     const nombre = document.getElementById('nombre').value;
     const descripcion = document.getElementById('descripcion').value;
